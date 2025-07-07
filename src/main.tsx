420e756bc5b43c4918b0b15b9a7fec054a6372ad
Full-Gor <arnaudbarotteaux@gmail.com>
2025-07-07 10:22:39 +0200

    fix connexion variable

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+console.log('Supabase connecté :', supabase);
+
+
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
